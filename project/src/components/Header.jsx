646f3d6694c044a979b5abe890a1a5c4bca5affb
bro-abu <a.komilovvv@gmail.com>
2023-03-26 19:57:18 +0500

    vrbs

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
-import logo from './img/logo.png'
-import HeaderContent from './img/header.png'
+import React from "react";
+import logo from "./img/logo.png";
+import HeaderContent from "./img/header.png";
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${HeaderContent})`,
+        backgroundPositionX: "530px",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <div className="navbar">
         <a href="#">
-        <img src={logo}/>
-        <p> Zone.</p> 
+          <img src={logo} />
+          <p> Zone.</p>
         </a>
-
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About us</a></li>
-          <li><a href="#">Service</a></li>
-          <li><a href="#">Pricing</a></li>
-          <li><a href="#">Blog</a></li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">Service</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
           <button>Contact us</button>
         </ul>
-
       </div>
 
-
+      <div className="content">
+        <h1>Virtual Reality Business Solutions</h1>
+        <p>
+          We have over 15 year exprience in business consultting arena. We have
+          over 15 year exprience in business consultting arena and artficial
+          intelligence.
+        </p>
+        <button>Join us</button> <button> Watch video</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
