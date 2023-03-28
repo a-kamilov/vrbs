1028cfedf9a61199037be8e9ae0c42df1a5190ff
bro-abu <a.komilovvv@gmail.com>
2023-03-28 18:01:21 +0500

    changes

--- project/src/components/Footer.jsx
+++ project/src/components/Footer.jsx
@@ -4,6 +4,20 @@ import logo from './img/logo.png'
 const Footer = () => {
   return (
     <div className='footer'>
+      <div className="links">
+        <div className="section">
+          <ul>
+            <li><a href="#" className='logo'><img src={logo}/>Zone.</a></li>
+            <li><a href="#"><i class="fas fa-map-marker-alt"></i>Dhaka, Bangladesh</a></li>
+            <li><a href="#"><i class="fa-solid fa-phone"></i>0943833399</a></li>
+            <li><a href="#"><i class="fa-solid fa-envelope"></i>support@zone.com</a></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </div>
+      
+      </div>
+
+
     </div>
   )
 }
--- project/src/components/News.jsx
+++ project/src/components/News.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const News = () => {
   return (
     <div className='news'>
-      
+      <h1>Subscribe to get the Latest News</h1>
+      <p>We recommended you to subscribe to our newspaper,drop your email below to get daily update about us</p>
+      <form>
+        <input type="email" placeholder='Enter your email addres'/>
+        <button><i class="fa-brands fa-telegram"></i>Subscribe</button>
+      </form>
     </div>
   )
 }
